f9e8dacb8d01d65f0f914445f20a37d4a96a1e19
Fran <fravelgue@gmail.com>
2020-02-12 12:45:25 +0100

    Add code-behind for WebPages
    via: http://www.compiledthoughts.com/2011/01/aspnet-mvc3-creating-razor-view-engine.html

---
+++ Pages/Default.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace AspNetWebPages.Pages
+{
+    //http://www.compiledthoughts.com/2011/01/aspnet-mvc3-creating-razor-view-engine.html
+    //Nesting files manualmente:
+    //<Compile Include="Pages\Default.cshtml.cs">
+    //  <DependentUpon>Default.cshtml</DependentUpon>
+    //</Compile>
+    // o https://marketplace.visualstudio.com/items?itemName=MadsKristensen.FileNesting
+
+    public abstract class DefaultPage : WebPage
+    {
+        public string Name { get { return "from code-behind"; } }
+    }
+}
