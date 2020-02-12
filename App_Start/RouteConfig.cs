a7af92472bdc5c6ae1847f32c3bc95cff4b670da
Fran <fravelgue@gmail.com>
2020-02-12 12:38:17 +0100

    ASP.NET WebPages Routing
    from https://bitbucket.org/pranavkm/routing

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+using System.Web.Routing;
+
+namespace AspNetWebPages
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            routes.MapWebPageRoute("", "~/Pages/Default.cshtml");
+            routes.MapWebPageRoute("{page}", "~/Pages/{page}.cshtml");
+        }
+    }
+}
